607a88eea999b9cb66d58dd4eee5bf1c22530aad
Vitalij <smorodnikov18052002@gmail.com>
2023-07-02 15:50:22 +0800

    new1

--- task_1/solution.js
+++ task_1/solution.js
@@ -3,7 +3,7 @@ function addInCartMessage(title, price) {
     let productPrice = price;
 
     // Задание №1.1. Оповещение о добавлении в корзину
-    let message = productName "за" productPrice "теперь в корзине!";
+    let message = productName 'за' productPrice 'теперь в корзине!' ;
     console.log(message);
     // создайте переменную message
     // присвойте переменной message значение переменной productName
