38861f10b93b7c530ccb5de2c7495288fea23161
Виталий <smorodnikov18052002@gmail.com>
2023-07-11 11:33:22 +0800

    new7

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,25 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = { goods: [], order: {} };
+    let data = {
+        client: name + ' ' + phone,
+        order: {
+            address: address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, кв. ' + address.flat,
+            sum: sum
+        },
+        goods: []
+    };
 
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i < countOfGoods; i += 1) {
-        data.goods.push({ title: goods[i].title, count: goods[i].count });
+    for (let i = 0; i < goods.length; i++) {
+        let good = {
+            title: goods[i].title,
+            count: goods[i].count
+        };
+        data.goods.push(good);
     }
-
-    data.order.address = address;
-    data.order.sum = sum;
-
-    data.client = name + ' ' + phone;
 
     let jsonData = JSON.stringify({ data });
 
     return jsonData;
 }
+
